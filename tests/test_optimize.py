5e0cbe6b4611c47619bda91965c4905af47b3001
Matteo Santamaria <matteosantama@gmail.com>
2022-12-29 08:50:55 -0500

    Prepare for v0.1.0 release (#12)

--- tests/test_optimize.py
+++ tests/test_optimize.py
@@ -54,3 +54,46 @@ def test_non_standard_variables() -> None:
     assert solution[x] == -2.0
     assert solution[y] == 4.0
     assert solution[z] == -1.0
+
+
+def test_inventory_balance_problem() -> None:
+    p = [0.5, 3.5, 5.0]
+    h = [1.0, 5.5, 1.5]
+    d = [50, 75, 100]
+
+    d_1, d_2, d_3 = d
+
+    x_1 = dz.Variable.nonneg()
+    x_2 = dz.Variable.nonneg()
+    x_3 = dz.Variable.nonneg()
+    x = [x_1, x_2, x_3]
+
+    z_1 = dz.Variable.nonneg()
+    z_2 = dz.Variable.nonneg()
+    z_3 = dz.Variable.nonneg()
+    z = [z_1, z_2, z_3]
+
+    purchase_cost = sum(p_t * x_t for p_t, x_t in zip(p, x))
+    inventory_holding_cost = sum(h_t * z_t for h_t, z_t in zip(h, z))
+
+    assert isinstance(purchase_cost, dz.model.AffExpr)
+    assert isinstance(inventory_holding_cost, dz.model.AffExpr)
+
+    total_cost = purchase_cost + inventory_holding_cost
+
+    problem = dz.Minimize(total_cost).subject_to(
+        [
+            x_1 >= d_1,
+            x_2 + z_1 >= d_2,
+            x_3 + z_2 >= d_3,
+            z_1 == x_1 - d_1,
+            z_2 == x_2 + z_1 - d_2,
+            z_3 == x_3 + z_2 - d_3,
+        ]
+    )
+    soln = problem.solve()
+
+    assert soln.objective_value == 637.5
+    assert soln[x_1] == 125.0
+    assert soln[x_2] == 0.0
+    assert soln[x_3] == 100.0
